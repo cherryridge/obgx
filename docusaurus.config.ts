e0d7b935e74dbf5e14a3fe001641e1b3b8621b29
LJM12914 <LJM12914@outlook.com>
2026-07-14 17:39:16 +0800

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,125 @@
+import {themes as prismThemes} from "prism-react-renderer";
+import type {Config} from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config :Config = {
+    title: "OBGX Documentation",
+    tagline: "Open Block Game Extension Interface",
+    favicon: "img/favicon.ico",
+
+    future: {
+        v4: true
+    },
+
+    url: "https://obgx.org",
+    baseUrl: "/",
+
+    trailingSlash: false,
+
+    onBrokenLinks: "throw",
+
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "zh-hans"]
+    },
+
+    markdown: {
+        mermaid: true,
+        hooks: {
+            onBrokenMarkdownLinks: "warn"
+        }
+    },
+
+    presets: [[
+        "classic", {
+            docs: {
+                routeBasePath: "/",
+                sidebarPath: "./sidebars.ts"
+            },
+            blog: {
+                routeBasePath: "/blog",
+                showReadingTime: true
+            },
+            theme: { customCss: "./src/custom.css" }
+        } satisfies Preset.Options
+    ]],
+
+    themeConfig: {
+        image: 'img/docusaurus-social-card.jpg',
+        colorMode: {
+            respectPrefersColorScheme: true,
+        },
+        navbar: {
+            title: 'My Site',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial',
+                },
+                {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://github.com/facebook/docusaurus',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'X',
+                            href: 'https://x.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
